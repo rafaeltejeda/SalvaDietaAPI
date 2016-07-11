6d45ca23532b0e2acc099f95c0a3779f3ebe97f8
rafae <rafae@DESKTOP-4USA2RE>
2016-07-11 16:32:51 -0300

    up

--- SalvaDieta.Ionic/www/js/controllers/configCtrl.js
+++ SalvaDieta.Ionic/www/js/controllers/configCtrl.js
@@ -2,10 +2,12 @@
     'use strict';
     angular.module('app').controller('configCtrl', configCtrl);
 
-    configCtrl.$inject = ['$scope', '$rootScope', '$timeout', '$ionicLoading', '$state' ]
+    configCtrl.$inject = ['$scope', '$rootScope', '$timeout', '$ionicLoading', '$state', 'userFactory' ]
 
-    function configCtrl($scope, $rootScope, $timeout, $ionicLoading, $state) {       
-        
+    function configCtrl($scope, $rootScope, $timeout, $ionicLoading, $state, userFactory) {
+
+        $scope.User = {};
+
         activate()
 
         function activate() {
@@ -13,8 +15,44 @@
              if ($rootScope.user == null) {
                     $state.go('login');
              }
-        }    
 
-         
+             getByEmail();
+        }
+
+
+        function getByEmail()
+        {
+             userFactory.getByEmail()
+             .success(success)
+             .catch(fail);
+
+             function success(response) {
+
+                      // Setup the loader
+                      $scope.loading = $ionicLoading.show({
+                            content: 'Loading',
+                            template: '<p class="item-icon-center"><ion-spinner icon="lines" class="spinner-calm"></ion-spinner></p>Carregando seus dados...',
+                            animation: 'fade-in',
+                            showBackdrop: true,
+                            maxWidth: 200,
+                            showDelay: 0
+                      });
+
+                      // Set a timeout to clear loader, however you would actually call the $scope.loading.hide(); method whenever everything is ready or loaded.
+                      $timeout(function () {
+                            $scope.user = response;
+                            console.log(response);
+                            $ionicLoading.hide();
+                      }, 2000);
+             }
+
+             function fail(error) {
+                      if (error.status == 401)
+                           swal("Você não tem permissão para ver essa página", 'Requisição não autorizada.', "error");
+                      else
+                      swal("Sua requisição não pode ser processada", 'Falha na requisição.', "error");
+            }
+        }
+
     };
 })();
